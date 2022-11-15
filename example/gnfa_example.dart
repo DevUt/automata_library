e0ffd30f6a7067d3714450183f20feeebddee93e
Utkarsh Tiwari <utkarsh02t@gmail.com>
2022-11-15 23:02:52 +0530

    Support Initializing NFA and GNFA from DFA

--- example/gnfa_example.dart
+++ example/gnfa_example.dart
@@ -1,6 +1,19 @@
+import 'package:automata_library/src/fa/dfa.dart';
 import 'package:automata_library/src/fa/gnfa.dart';
 
 void main() {
+  DFA dfa = DFA(
+      alphabet: {"0", "1"},
+      initialState: "q0",
+      acceptingStates: {"q0"},
+      states: {"q0", "q1"},
+      transitionFunction: {
+        "q0": {"0": "q1", "1": "q0"},
+        "q1": {"0": "q0", "1": "q1"}
+      });
+
+  print(GNFA.fromDFA(dfa).regex());
+
   GNFA gnfa = GNFA(
       alphabet: {'0', '1'},
       states: {'q0', 'q1', 'q2'},
