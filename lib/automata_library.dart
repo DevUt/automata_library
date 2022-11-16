a44e452d1418ea4caea0073f4bbfd3db4d0c611d
Parth Shah <shahparth200212@gmail.com>
2022-11-16 21:53:10 +0530

    exported gnfa.dart

--- lib/automata_library.dart
+++ lib/automata_library.dart
@@ -3,6 +3,7 @@ library automata_library;
 
 export 'src/fa/dfa.dart';
 export 'src/fa/nfa.dart';
+export 'src/fa/gnfa.dart';
 
 export 'src/base/helper_classes.dart';
 export 'src/base/exceptions.dart';
